b3f0473ca5bbc1709b73d39ee2b7ff76a274aba4
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-10-17 13:54:04 -0300

    Comentando o código

--- Projeto.py
+++ Projeto.py
@@ -1,9 +1,14 @@
 class Projeto:
-    PAYBACK = 3
-    CONSUMOENERGIA = 15000
-    GERACAOENERGIA = 15000 * 0.80
+    # Parâmetros definidos pela empresa para comparação
+    GERACAO_CO2: float = 784.5
+    PAYBACK: int = 3
+    CONSUMOENERGIA:float = 50000
+    GERACAOENERGIA: float = 50000 * 0.80 # 80% do consumo de energia 
+    REDUCAOCO2: float = 1000
 
+    # Construtor
     def __init__(self, nome: str, payback: int, geracaoEnergia: float) -> None:
         self.nome = nome
         self.payback = payback
         self.geracao = geracaoEnergia
+        self.geracaoCo2 = geracaoEnergia * 0.035
--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 from Projeto import Projeto
 
 def avaliaProjeto(projeto: Projeto) -> str:
+    '''Avalia o projeto de acordo com as metas já propostas pela empresa.
+    Args:
+        projeto (Projeto): O objeto projeto com todos os seus atributos.
+    Returns:
+        str: Retorna uma mensagem dizendo a qualidade do projeto.
+    '''
     pontos = 0
-    if projeto.payback >= Projeto.PAYBACK:
-        pontos += 2
-    if projeto.geracao >= Projeto.GERACAOENERGIA:
-        pontos +=1
 
+    # Compara os parâmetros do projeto pelas metas da empresa.
+    if projeto.payback >= Projeto.PAYBACK:
+        pontos += 1
+    if projeto.geracao >= Projeto.GERACAOENERGIA:
+        pontos += 1
+    if projeto.geracaoCo2 >= Projeto.REDUCAOCO2:
+        pontos += 1
+    
+    # Verifica o nível do projeto
     if pontos >= 3:
-        return "Um ótimo projeto"
-    elif pontos >= 1:
+        return "Um projeto ótimo"
+    elif pontos >= 2:
         return "Um projeto mediano"
     else:
         return "Um projeto ruim"
 
 def definiProjeto() -> Projeto:
+    '''Define o projeto com os parâmetros recebidos pelo usuário.
+    Args:
+        Não possui argumentos.
+    Returns:
+        Projeto: Retorna o objeto Projeto criado.
+    '''
+    # Recebe os valores do usúario
     nome = input("Digite o nome do projeto: ")
     payback = int(input("Digite o tempo de retorno do projeto: "))
     geracaoEnergia = float(input("Digite a geração de energia do projeto: "))
+    # Cria o objeto do projeto
     projeto = Projeto(nome, payback, geracaoEnergia)
     return projeto
 
