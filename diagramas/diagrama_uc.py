f9487f6aaa73f3528a1c290f72fbcb898ecfbac5
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-11-01 18:38:30 -0300

    Add diagrama de casos de uso

---
+++ diagramas/diagrama_uc.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+from plantuml import PlantUML
+
+plantuml_code_string = """
+@startuml
+
+skinparam style strictuml
+skinparam Class {
+    BackgroundColor white
+    BorderColor black
+    IconFontSize 0
+}
+
+skinparam classAttributeIconSize 0
+
+' 1. Definição dos Atores
+actor Usuário
+
+' 2. Definição dos Casos de Uso
+package "Sistema de avaliação" {
+    usecase (UC01 - Inserir dados) as UC1
+}
+
+' 3. Relações entre Atores e Casos de Uso
+Usuário -r-> UC1
+
+@enduml
+"""
+
+plantuml = PlantUML(url='http://www.plantuml.com/plantuml/img/')
+imagem = plantuml.processes(plantuml_code_string)
+diretorioAtual = Path(__file__).resolve().parent
+nomeArquivo = diretorioAtual / 'diagrama_uc.png'
+with open(nomeArquivo, 'wb') as f:
+    f.write(imagem)
+
+print(f"Diagrama salvo com sucesso em: {nomeArquivo}")
